4f98a0dd43df06d7af580b26cd8169d76966f3ec
iver56 <iver56@hotmail.com>
2013-07-11 21:02:24 +0200

    Implemented animation for the bodies. Moved parts (which will grow big) to a separate file.

---
+++ src/TrainParts.js
@@ -0,0 +1,282 @@
+var trainParts = [ {
+	name : 'front_left_wheel0',
+	loaded : false,
+	type : 'wheel',
+	offset : new THREE.Vector3(-19.15, 5, 0),
+	activeAnimation : -1
+},
+{
+	name : 'front_left_wheel1',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(-12.7, 5, 0)
+},
+{
+	name : 'front_right_wheel0',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(-19.15, 5, 0)
+},
+{
+	name : 'front_right_wheel1',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(-12.7, 5, 0)
+},
+{
+	name : 'middle_left_wheel0',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(-1.1, 5, 0)
+},
+{
+	name : 'middle_left_wheel1',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(5.4, 5, 0)
+},
+{
+	name : 'middle_right_wheel0',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(-1.1, 5, 0)
+},
+{
+	name : 'middle_right_wheel1',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(5.4, 5, 0)
+},
+{
+	name : 'rear_left_wheel0',
+	loaded : false,
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(16.6, 5, 0)
+},
+{
+	loaded : false,
+	name : 'rear_left_wheel1',
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(23.1, 5, 0)
+},
+{
+	loaded : false,
+	name : 'rear_right_wheel0',
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(16.6, 5, 0)
+},
+{
+	loaded : false,
+	name : 'rear_right_wheel1',
+	type : 'wheel',
+	activeAnimation : -1,
+	offset : new THREE.Vector3(23.1, 5, 0)
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'chimney'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'cube0'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'cube1'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'cube2'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'cube3'
+},
+{
+	name : 'front_body',
+	loaded : false,
+	activeAnimation : 0,
+	animations : [ {
+		start : 0,
+		end : 1000,
+		fromPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		},
+		toPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		}
+	},
+		{
+			start : 1000,
+			end : 3000,
+			fromPos : {
+				x : 0,
+				y : 50,
+				z : 0
+			},
+			toPos : {
+				x : 0,
+				y : 0,
+				z : 0
+			}
+		},
+	]
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'front_bullet'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'front_plate'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'lower_plate'
+},
+{
+	loaded : false,
+	name : 'middle_body',
+	activeAnimation : 0,
+	animations : [ {
+		start : 0,
+		end : 2000,
+		fromPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		},
+		toPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		}
+	},
+		{
+			start : 2000,
+			end : 4000,
+			fromPos : {
+				x : 0,
+				y : 50,
+				z : 0
+			},
+			toPos : {
+				x : 0,
+				y : 0,
+				z : 0
+			}
+		},
+	]
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'middle_plate'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole0'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole1'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole2'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole3'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole4'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'pole5'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'rear_body',
+	activeAnimation : 0,
+	animations : [ {
+		start : 0,
+		end : 3000,
+		fromPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		},
+		toPos : {
+			x : 0,
+			y : 50,
+			z : 0
+		}
+	},
+		{
+			start : 3000,
+			end : 5000,
+			fromPos : {
+				x : 0,
+				y : 50,
+				z : 0
+			},
+			toPos : {
+				x : 0,
+				y : 0,
+				z : 0
+			}
+		},
+	]
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'roof1'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'roof2'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'roof3'
+},
+{
+	loaded : false,
+	activeAnimation : -1,
+	name : 'upper_plate'
+} ];
--- src/TrainScene.js
+++ src/TrainScene.js
@@ -3,184 +3,6 @@ function TrainScene(){
     this.startTime = 0;
     /* short name of this scene, must be defined */
     this.NAME = 'train';
-	
-	this.parts = [
-          {
-        	  name:'front_left_wheel0',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-19.15,5,0),
-        	  activeAnimation: 0,
-        	  animation: [
-	              {
-	            	  start: 0,
-	            	  end: 2000,
-	            	  fromPos: {
-	            		  x: 0,
-	            		  y: 0,
-	            		  z: 20
-	            	  },
-	            	  toPos: {
-	            		  x: 0,
-	            		  y: 0,
-	            		  z: 0
-	            	  }
-	              },
-              ]
-          },
-          {
-        	  name:'front_left_wheel1',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-12.7,5,0)
-          },
-          {
-        	  name:'front_right_wheel0',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-19.15,5,0)
-          },
-          {
-        	  name:'front_right_wheel1',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-12.7,5,0)
-          },
-          {
-        	  name:'middle_left_wheel0',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-1.1,5,0)
-          },
-          {
-        	  name:'middle_left_wheel1',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(5.4,5,0)
-          },
-          {
-        	  name:'middle_right_wheel0',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(-1.1,5,0)
-          },
-          {
-        	  name:'middle_right_wheel1',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(5.4,5,0)
-          },
-          {
-        	  name:'rear_left_wheel0',
-        	  loaded:false,
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(16.6,5,0)
-          },
-          {
-        	  loaded:false,
-        	  name:'rear_left_wheel1',
-        	  type:'wheel',
-        	  offset: new THREE.Vector3(23.1,5,0)
-          },
-          {
-        	  loaded:false,
-        	  name:'rear_right_wheel0',
-        	  type:'wheel',   offset: new THREE.Vector3(16.6,  5,0)},
-          {
-              loaded: false,
-              name: 'rear_right_wheel1',
-              type: 'wheel',
-              offset: new THREE.Vector3(23.1, 5, 0)
-          },
-          {
-              loaded: false,
-              name: 'chimney'
-          },
-          {
-              loaded: false,
-              name: 'cube0'
-          },
-          {
-              loaded: false,
-              name: 'cube1'
-          },
-          {
-              loaded: false,
-              name: 'cube2'
-          },
-          {
-              loaded: false,
-              name: 'cube3'
-          },
-          {
-              loaded: false,
-              name: 'front_body'
-          },
-          {
-              loaded: false,
-              name: 'front_bullet'
-          },
-          {
-              loaded: false,
-              name: 'front_plate'
-          },
-          {
-              loaded: false,
-              name: 'lower_plate'
-          },
-          {
-              loaded: false,
-              name: 'middle_body'
-          },
-          {
-              loaded: false,
-              name: 'middle_plate'
-          },
-          {
-              loaded: false,
-              name: 'pole0'
-          },
-          {
-              loaded: false,
-              name: 'pole1'
-          },
-          {
-              loaded: false,
-              name: 'pole2'
-          },
-          {
-              loaded: false,
-              name: 'pole3'
-          },
-          {
-              loaded: false,
-              name: 'pole4'
-          },
-          {
-              loaded: false,
-              name: 'pole5'
-          },
-          {
-              loaded: false,
-              name: 'rear_body'
-          },
-          {
-              loaded: false,
-              name: 'roof1'
-          },
-          {
-              loaded: false,
-              name: 'roof2'
-          },
-          {
-              loaded: false,
-              name: 'roof3'
-          },
-          {
-              loaded: false,
-              name: 'upper_plate'
-          }
-      ];
 }
 
 TrainScene.prototype.init = function(cb){
@@ -222,11 +44,11 @@ TrainScene.prototype.init = function(cb){
 				}
 			});
 			var everythingIsLoaded = true;
-			for (var i = 0; i < that.parts.length; i++) {
-				if (that.parts[i].name === name) {
-					that.parts[i].loaded = true;
-					if (that.parts[i].type === 'wheel') {
-						var offset = that.parts[i].offset;
+			for (var i = 0; i < trainParts.length; i++) {
+				if (trainParts[i].name === name) {
+					trainParts[i].loaded = true;
+					if (trainParts[i].type === 'wheel') {
+						var offset = trainParts[i].offset;
 						var pivot = new THREE.Object3D();
 						pivot.position.x = -offset.x;
 						pivot.position.y = -offset.y;
@@ -242,20 +64,20 @@ TrainScene.prototype.init = function(cb){
 						that.scene.add(object);
 					}
 				}
-				if (!that.parts[i].loaded) {
+				if (!trainParts[i].loaded) {
 					everythingIsLoaded = false;
 				}
 			}
 			if (everythingIsLoaded) {
-				//console.log("all objects are loaded");
+				console.log("all train parts are loaded");
 				cb();	//done with the loading
 			}
 		});
 		loader.load(objPath);
 	};
 	
-	for (var i = 0; i < this.parts.length; i++) {
-		var partName = this.parts[i].name;
+	for (var i = 0; i < trainParts.length; i++) {
+		var partName = trainParts[i].name;
 		var objPath = resourceFolderPath + partName + '.obj';
 		loadObject(objPath, partName);
 	}
@@ -272,17 +94,32 @@ TrainScene.prototype.update = function(){
 	//this.camera.position.y += 0.01;
 	this.camera.lookAt(this.objects['roof1'].position);
 	
-	for (var i = 0; i < this.parts.length; i++) {
-		var object = this.objects[this.parts[i].name];
-		var part = this.parts[i];
+	for (var i = 0; i < trainParts.length; i++) {
+		var object = this.objects[trainParts[i].name];
+		var part = trainParts[i];
 		if (part.type === 'wheel') {
 			object.rotation.z -= 0.06*Math.PI;
 		}
 		if (part.activeAnimation >= 0) {
 			var animation = part.animations[part.activeAnimation];
-			
-			if (++part.activeAnimation === part.animations.length) {
-				part.activeAnimation = -1;
+			if (t >= animation.start) {
+				var length = animation.end - animation.start;
+				var animationProgress = Math.min((t - animation.start) / length, 1); //number between 0 and 1
+				
+				
+				var x = smoothstep(animation.fromPos.x, animation.toPos.x, animationProgress);
+				var y = smoothstep(animation.fromPos.y, animation.toPos.y, animationProgress);
+				var z = smoothstep(animation.fromPos.z, animation.toPos.z, animationProgress);
+				
+				object.position.x = x;
+				object.position.y = y;
+				object.position.z = z;
+				
+				//if animation is done, go to next animation for this object if available
+				if (t > animation.end && ++part.activeAnimation === part.animations.length) {
+					console.log("an animation has ended at " + t);
+					part.activeAnimation = -1;
+				}
 			}
 		}
 	}
